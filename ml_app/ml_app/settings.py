bd23f8f9bd7cad92f626d05c267fbc524ac4eaac
Johnny <johnny.liang@ontariotechu.net>
2024-12-04 14:50:11 -0500

    small commit - will not push

--- ml_app/ml_app/settings.py
+++ ml_app/ml_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,10 @@ ROOT_URLCONF = 'ml_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            BASE_DIR / 'base/templates/base',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
